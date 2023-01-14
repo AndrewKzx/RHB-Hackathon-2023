377c18a1bb16e7f12fa07c2e85181185b9ac5370
AndrewKzx <khorzx01@gmail.com>
2023-01-14 10:53:57 +0800

    tailwindcss

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <h1>Testing2</h1>
+    <h1 class="m-6 font-bold" style="font-size:20px">Testing2</h1>
+    <p style="font-size:200px">Testing 3000 p</p>
+    <h4> Testing 4</h4>
   </div>
 </template>
 
