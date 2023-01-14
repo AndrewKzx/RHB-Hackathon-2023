029005452f7abda0c1dc60588b9b59335b800864
LorenzoPixel <71122234+HealthyFryOption@users.noreply.github.com>
2023-01-14 23:17:11 +0800

    newer
    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        RHDarkBlue: "#1e6bb0",
         RHBlue: "#8bd0e5",
-        GreenText: "80d491",
-        RedText: "ff0022",
-        GreyText: "9d9d9d",
-        RHGrey: "EBE7EB",
-        RHDarkBlue: "1e6bb0",
+        GreenText: "#80d491",
+        RedText: "#ff0022",
+        GreyText: "#9d9d9d",
+        RHGrey: "#EBE7EB",
       },
     },
   },
